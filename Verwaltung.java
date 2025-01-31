aca79a68eaf10164eee29fb5e478e7908c52bd96
MajorityBard <sven@hartmann.vc>
2025-01-31 10:38:47 +0100

    Weitergemacht

--- Verwaltung.java
+++ Verwaltung.java
@@ -12,14 +12,17 @@ public class Verwaltung {
     }
     public Verwaltung(){
         scanner = new Scanner(System.in);
+
         Warteschlange warteschlange= new Warteschlange();
 
         while (true) {
             System.out.println("== HAUPTMENÜ ==");
             System.out.println("[1]Hinzufuegen");
-            System.out.println("[2]Entfernen");
-            System.out.println("[3]ZeigeInhalt");
+            System.out.println("[2]Manuell Hinzufuegen");
+            System.out.println("[3]Entfernen");
+            System.out.println("[4]ZeigeInhalt");
             int option = scanner.nextInt();
+            scanner.nextLine();
             if(option==1) {
 
                 System.out.println("Wie viele willst du hinzufuegen?");
@@ -29,10 +32,18 @@ public class Verwaltung {
                     warteschlange.einfuegen(Speicher);
 
                 }
-            }else if (option==2){
-                warteschlange.entfernen();
+            }
+            else if (option==2){
+                System.out.println("Namen angeben");
+                String OptionString = scanner.nextLine();
+                Kunde SpeicherM = new Kunde(OptionString);
+                warteschlange.einfuegen(SpeicherM);
+
             }
             else if (option==3){
+                warteschlange.entfernen();
+            }
+            else if (option==4){
                 Kunde hilfe = (Kunde) warteschlange.gibErsten();
                 System.out.println(hilfe.getName());}
 
